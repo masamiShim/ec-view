2510a3be30efb7f7add1c630c3e2f77cc3a92d95
shinbuichi <shinbuichi@iij.ad.jp>
2019-09-03 13:15:44 +0900

    initial commit

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import OwnerLogin from '@/views/OwnerLogin.vue';
+import UserLogin from '@/views/UserLogin.vue';
 
 Vue.use(Router);
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/login/owner',
+      name: 'OwnerLogin',
+      component: OwnerLogin,
+    },
+    {
+      path: '/login/user',
+      name: 'UserLogin',
+      component: UserLogin,
     },
     {
       path: '/about',
---
+++ src/views/UserLogin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+  @Component({
+    name: 'UserLogin',
+  })
+export default class UserLogin extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
