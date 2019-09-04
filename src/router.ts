66251487d0b62645fea661150d79f9f61ff48dd6
shinbuichi <shinbuichi@iij.ad.jp>
2019-09-04 18:39:52 +0900

    ファイルアップロード機能追加

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import OwnerLogin from '@/views/OwnerLogin.vue';
 import UserLogin from '@/views/UserLogin.vue';
-import PropertyRegister from '@/views/owner/ItemRegister.vue';
+import ItemRegister from '@/views/owner/ItemRegister.vue';
 
 Vue.use(Router);
 
@@ -20,9 +20,9 @@ export default new Router({
       component: OwnerLogin,
     },
     {
-      path: '/owner/property/register',
-      name: 'PropertyRegister',
-      component: PropertyRegister,
+      path: '/owner/item/register',
+      name: 'ItemRegister',
+      component: ItemRegister,
     },
     {
       path: '/login/user',
--- src/views/owner/ItemRegister.vue
+++ src/views/owner/ItemRegister.vue
@@ -19,42 +19,45 @@
               dark
               flat
             >
-              <v-toolbar-title>アイテム登録</v-toolbar-title>
+              <v-toolbar-title>商品登録</v-toolbar-title>
               <div class="flex-grow-1"></div>
             </v-toolbar>
             <v-card-text>
               <v-form>
                 <v-text-field
-                  label="Name"
+                  label="商品名"
                   name="name"
                   :error-messages="nameErrors"
                   v-model="form.name"
                   type="text"
                 ></v-text-field>
                 <v-text-field
-                  label="Code"
+                  label="商品コード"
                   name="code"
                   v-model="form.code"
                   type="text"
                 ></v-text-field>
                 <v-text-field
-                  label="Price"
+                  label="金額"
                   name="price"
                   v-model="form.price"
                   type="number"
                 ></v-text-field>
                 <v-text-field
-                  label="Quantity"
+                  label="数量"
                   name="quantity"
                   v-model="form.quantity"
                   type="number"
                 ></v-text-field>
                 <v-text-field
-                  label="Comment"
+                  label="コメント"
                   name="comment"
                   v-model="form.comment"
                   type="text"
                 ></v-text-field>
+                <v-file-input label="商品画像" show-size chips multiple accept="image/*" @change="onFileChange">
+
+                </v-file-input>
               </v-form>
             </v-card-text>
             <v-card-actions class="pb-4">
@@ -70,35 +73,56 @@
 
 <script lang="ts">
 
-  import {Vue, Component} from 'vue-property-decorator';
-  import ItemRegisterForm from "../../api/ItemRegisterForm";
-  import {required} from "vuelidate/lib/validators";
-
+  import { Vue, Component } from 'vue-property-decorator';
+  import ItemRegisterForm from '../../api/ItemRegisterForm';
+  import { between, maxLength, required } from 'vuelidate/lib/validators';
 
   @Component({
     validations: {
       form: {
-        name: { required }
+        name: { required, maxLength: maxLength(80) },
+        code: { maxLength: maxLength(20) },
+        price: { required, between: between(0, 100000000) },
+        quantity: { required, between: between(1, 100000000) },
+        comment: { maxLength: maxLength(1000) }
       }
-    },
-    name: "ItemRegister"
+    }, name: 'ItemRegister'
   })
   export default class ItemRegister extends Vue {
     form: ItemRegisterForm = {
-      name: '',
-      code: '',
-      price: 0,
-      quantity: 0,
-      comment: ''
-    }
+      name: '', code: '', price: 0, quantity: 0, comment: ''
+    };
 
-    doRegister() {
+    selectedImages: File[] = [];
+    selectedImagesPath: any[] = [];
+
+    doRegister () {
+      (this as any).$v.$touch();
       console.log('register');
     }
 
-    get nameErrors() {
+    onFileChange (files: File[]) {
+
+      for (let i = 0, file: File; file = files[i]; i++) {
+        const reader: FileReader = new FileReader();
+
+        reader.onload = ((ev: ProgressEvent<FileReader>) => {
+          const f = ev.target.result;
+          if (!f) {
+            return;
+          }
+          console.log("onloaded: " + f);
+          this.selectedImages.push(file);
+          this.selectedImagesPath.push(f);
+        });
+
+        reader.readAsDataURL(file);
+      }
+    }
+
+    get nameErrors () {
       const errors = [];
-      !(this as any).$v.form.name.required && errors.push( "名前な必須です。" );
+      !(this as any).$v.form.name.required && errors.push('名前は必須です。');
       return errors;
     };
   }
